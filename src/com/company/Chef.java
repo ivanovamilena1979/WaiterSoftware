06a1eaae9af06abd05d8e0c04c10b6a404131efd
DesislavaZ <d_samfirova@abv.bg>
2021-11-29 19:08:14 +0200

    change menu

--- src/com/company/Chef.java
+++ src/com/company/Chef.java
@@ -12,9 +12,9 @@ public class Chef extends Employee {
     public void getMenu() {
         Scanner userMenu = new Scanner(System.in);
         System.out.println("1. View order");
-        System.out.println("2 Return to Login menu");
-        System.out.println("2. Finish order");
-        System.out.println("3. Quit");
+        System.out.println("2 Finish orderReturn to Login menu");
+        System.out.println("3. Return to Login menu");
+        System.out.println("4. Quit");
 
         do {
             selection = userMenu.nextInt();
