cf3a4873cacdff3a8537bf111df02c70849c3d25
ivanovamilena1979 <ivanovamilena1979@gmail.com>
2021-11-30 17:42:23 +0200

    The RestaurantMenu changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-Login login=new Login();
-login.login();
+    public static void main(String[] args) throws IOException {
+//        Login login = new Login();
+//        login.login();
+        RestaurantMenu menu = new RestaurantMenu();
+        menu.readMenu();
+        //menu.addFoodMenu();
     }
 }
--- src/com/company/RestaurantMenu.java
+++ src/com/company/RestaurantMenu.java
@@ -4,69 +4,71 @@ import java.io.*;
 import java.util.Scanner;
 
 public class RestaurantMenu implements MenuInterface {
+    private int itemNumber;
+    private String productType;
+    private String productName;
+    private double productPrice;
 
-    private String foodType;
-    private String foodName;
-    private double foodPrice;
 
+//    public Menu(String foodType, String foodName, double foodPrice) {
+//        this.foodType = foodType;
+//        this.foodPrice = foodPrice;
+//        this.foodName = foodName;
+//    }
 
-    public RestaurantMenu(String foodType, String foodName, double foodPrice) {
-        this.foodType = foodType;
-        this.foodPrice = foodPrice;
-        this.foodName = foodName;
+    public String getproductType() {
+        return productType;
     }
 
-    public String getfoodType() {
-        return foodType;
+    public String getproductName() {
+        return productName;
     }
 
-    public String getfoodName() { return foodName; }
-
-    public double getfoodPrice() {
-        return foodPrice;
+    public double getproductPrice() {
+        return productPrice;
     }
 
 
     @Override
     public String toString() {
-        return String.format(getfoodName() + "  " + getfoodType() + "  price: " + getfoodPrice() + "\n");
+        return String.format( getproductName() + "  " + getproductType() + "  price: " + getproductPrice() + "\n" );
     }
 
     @Override
-    public void readMenu() throws FileNotFoundException {
-            File file = new File("Menu.txt");
-            Scanner fileReader = new Scanner(file, "windows-1251");
-            int lineNumber = 0;
-            while (fileReader.hasNext()){
-                lineNumber++;
-                if (lineNumber % 2 != 0){
-                    System.out.println("Тип : " + fileReader.nextLine());
-                }
-                else{
-                    System.out.println("Цена : " + fileReader.nextLine());
-                }
-            }
-            fileReader.close();
-        }
+    public void readMenu() throws IOException {
+        //File file = new File( "C:\\Users\\Lenovo\\Desktop\\menu1.txt" );
+        //Scanner fileReader = new Scanner( file, "windows-1251" );
 
+        FileReader fin = new FileReader( "C:\\Users\\Lenovo\\Desktop\\menu1.txt" );
+        Scanner fileReader = new Scanner( fin );
+
+        int lineNumber = 0;
+        while (fileReader.hasNext()) {
+            lineNumber++;
+            if (lineNumber % 2 != 0) {
+                System.out.println( fileReader.nextLine() );
+            } else {
+                System.out.println( fileReader.nextLine() );
+            }
+        }
+    }
 
     @Override
     public void addRestaurantMenu() throws IOException {
-        FileWriter fileWriter = new FileWriter("Menu.txt", true);
-        PrintWriter out = new PrintWriter(fileWriter);
-        Scanner input = new Scanner(System.in);
-        System.out.println("Въведете тип на продукта: ");
+        FileWriter fileWriter = new FileWriter( "C:\\Users\\Lenovo\\Desktop\\menu1.txt", true );
+        PrintWriter out = new PrintWriter( fileWriter );
+        Scanner input = new Scanner( System.in );
+        System.out.println( "Enter the type of the product (starter, salad,dessert or drink): " );
         try {
-            String foodType = input.nextLine();
-            System.out.println("Наименование на продукта: ");
-            String foodName = input.nextLine();
-            System.out.println("Въведете цена на продукта: ");
-            double foodPrice = input.nextDouble();
-            out.println("Тип продукт: " + foodType + " Име на продукт: " + foodName + " Цена на продукт: " + foodPrice);
+            String productType = input.nextLine();
+            System.out.println( "The name of the item: " );
+            String productName = input.nextLine();
+            System.out.println( "Enter the price of the item: " );
+            double productPrice = input.nextDouble();
+            out.println( "Product type: " + productType + " Product name: " + productName + " Price " + productPrice );
             out.close();
-        }
-        catch (Exception e){
-            System.out.println("Грешни входни данни!");
+        } catch (Exception e) {
+            System.out.println( "Wrong input!" );
         }
     }
 
